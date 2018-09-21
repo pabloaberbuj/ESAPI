54f9f0a5025ae6c66fe0e95b8b2d0f0fe4cd235a
Pablo Aberbuj <pabloaberbuj@gmail.com>
2018-09-21 12:08:12 -0300

    Probando serializar

---
+++ WinForms/SerializarObjetos/SerializarObjetos/Clase1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SerializarObjetos
+{
+    class Clase1
+    {
+        Clase2 clase2 { get; set; }
+        string nombre { get; set; }
+        List<Clase2> lista { get; set; }
+        double valor { get; set; }
+
+        public Clase1 crear(Clase2 _clase, string _nombre, double _valor)
+        {
+            List<Clase2> lista = new List<Clase2>();
+            lista.Add(_clase);
+            return new Clase1()
+            {
+                clase2 = _clase,
+                nombre = _nombre,
+                lista = lista,
+                valor = _valor,
+            };
+        }
+    }
+}
---
+++ WinForms/SerializarObjetos/SerializarObjetos/Clase2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SerializarObjetos
+{
+    public class Clase2
+    {
+        string nombre { get; set; }
+        string ID { get; set; }
+        double[] numeros { get; set; }
+        List<string> lista { get; set; }
+
+        public Clase2 crear(string _nombre, string _ID, double _numero, string listaEl)
+        {
+            double[] numeros = { _numero, _numero };
+            List<string> lista = new List<string>();
+            lista.Add(listaEl);
+            lista.Add(listaEl + "1");
+
+            return new Clase2()
+            {
+                nombre = _nombre,
+                ID = _ID,
+                numeros = numeros,
+                lista = lista,
+            };
+        }
+    }
+}
