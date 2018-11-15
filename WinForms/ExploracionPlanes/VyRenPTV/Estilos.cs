f89f38311752e1c533206c7fbb64a89cd683609d
pabloaberbuj <pabloaberbuj@gmail.com>
2018-11-15 17:00:57 -0300

    Nuevo Reporte

---
+++ WinForms/ExploracionPlanes/VyRenPTV/Estilos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Tables;
+
+namespace PruebaReporte
+{
+    internal class Estilos
+    {
+        public static void definirEstilos(Document doc)
+        {
+            var texto = doc.AddStyle("Texto", StyleNames.Normal);
+            texto.ParagraphFormat.Font.Name = "Arial";
+            texto.ParagraphFormat.Font.Color = Colors.Black;
+            texto.ParagraphFormat.Font.Size = 10;
+            texto.ParagraphFormat.Font.Bold = false;
+            texto.ParagraphFormat.Alignment = ParagraphAlignment.Left;
+            texto.ParagraphFormat.SpaceAfter = 6;
+
+            var textoNegrita = doc.AddStyle("Texto Negrita", "Texto");
+            textoNegrita.ParagraphFormat.Font.Bold = true;
+
+            var textoCentro = doc.AddStyle("Texto Centro", "Texto");
+            textoCentro.ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+            var titulo = doc.AddStyle("Titulo", "Texto");
+            titulo.ParagraphFormat.Font.Size = 18;
+            titulo.ParagraphFormat.Font.Bold = true;
+            titulo.ParagraphFormat.Alignment = ParagraphAlignment.Center;
+            titulo.ParagraphFormat.SpaceAfter = 16;
+
+            var subtitulo = doc.AddStyle("Subtitulo", "Texto");
+            subtitulo.ParagraphFormat.Font.Size = 13;
+            subtitulo.ParagraphFormat.Font.Bold = true;
+            subtitulo.ParagraphFormat.SpaceAfter = 10;
+
+            var tabla = doc.AddStyle("Tabla", "Texto");
+            tabla.ParagraphFormat.Font.Name = "Microsoft Sans Serif";
+            tabla.ParagraphFormat.Font.Size = 9;
+
+            var tablaHeader = doc.AddStyle("Tabla Header", "Tabla");
+            tablaHeader.ParagraphFormat.Font.Bold = true;
+        }
+        public static void formatearTabla(Table tabla)
+        {
+            tabla.Style = "Tabla";
+            tabla.TopPadding = 5;
+            tabla.BottomPadding = 5;
+
+            tabla.Borders.Visible = true;
+            tabla.Borders.Width = 0.25;
+            tabla.Borders.Color = Colors.Gray;
+            tabla.Format.Alignment = ParagraphAlignment.Right;
+            tabla.Columns[0].Format.Alignment = ParagraphAlignment.Left;
+            tabla.Rows[0].Format.Alignment = ParagraphAlignment.Center;
+            tabla.Rows[0].Borders.Bottom.Width = 1;
+            tabla.Rows[0].Borders.Bottom.Color = Colors.Black;
+            tabla.Rows[0].Style = "Tabla Header";
+        }
+
+        public static void formatearTablaEncabezados(Table tabla)
+        {
+            tabla.Columns[0].Style = "Texto";
+            tabla.Columns[1].Style = "Texto Negrita";
+            tabla.Borders.Visible = false;
+            
+        }
+    }
+}
---
+++ WinForms/ExploracionPlanes/VyRenPTV/Reporte.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+
+
+namespace PruebaReporte
+{
+    public static class Reporte
+    {
+        public static void exportarAPdf(string path, Document report)
+        {
+            PdfDocumentRenderer pdfRenderer = new PdfDocumentRenderer();
+            pdfRenderer.Document = report;
+            pdfRenderer.RenderDocument();
+            pdfRenderer.PdfDocument.Save(path);
+        }
+
+        public static Document crearReporte(Paciente paciente, Plantilla plantilla, DataGridView DGV)
+        {
+            var doc = new Document();
+            Estilos.definirEstilos(doc);
+            doc.Add(crearSeccion(paciente, plantilla, DGV));
+            return doc;
+        }
+
+        private static Section crearSeccion(Paciente paciente, Plantilla plantilla, DataGridView DGV)
+        {
+            Section seccion = new Section();
+            cargarEncabezado(seccion, paciente, plantilla);
+            cargarTabla(seccion, paciente, plantilla, DGV);
+            return seccion;
+        }
+
+        private static void cargarEncabezado(Section seccion, Patient paciente, Plantilla plantilla)
+        {
+            seccion.AddParagraph("Analisis de restricciones en plan de tratamiento", "Titulo");
+            MigraDoc.DocumentObjectModel.Tables.Table tabla = new MigraDoc.DocumentObjectModel.Tables.Table();
+            tabla.AddColumn(10);
+            tabla.AddColumn();
+            var fila1 = tabla.AddRow();
+            var fila2 = tabla.AddRow();
+            var fila3 = tabla.AddRow();
+            fila1[0].AddParagraph("Paciente:");
+            fila1[1].AddParagraph(apellidoPaciente, )
+
+
+
+            seccion.AddParagraph("Plantilla: ", "Texto");
+        }
+        private static MigraDoc.DocumentObjectModel.Tables.Table convertirDGVaTabla(DataGridView DGV)
+        {
+            MigraDoc.DocumentObjectModel.Tables.Table tabla = new MigraDoc.DocumentObjectModel.Tables.Table();
+            foreach (DataGridViewColumn columna in DGV.Columns)
+            {
+                tabla.AddColumn(columna.Width);
+            }
+            var filaHeadertabla = tabla.AddRow();
+            foreach (DataGridViewRow filaDGV in DGV.Rows)
+            {
+                var tableFila = tabla.AddRow();
+                for (int i = 0; i < DGV.ColumnCount; i++)
+                {
+                    tableFila.Cells[i].AddParagraph(filaDGV.Cells[i].Value.ToString());
+                    tableFila.Cells[i].Shading.Color = colorDGVaTable(filaDGV.Cells[i].Style.BackColor);
+                }
+            }
+            for (int i = 0; i < DGV.ColumnCount; i++)
+            {
+                tabla.Rows[0].Cells[i].AddParagraph(DGV.Columns[i].HeaderText);
+            }
+            Estilos.formatearTabla(tabla);
+            return tabla;
+
+        }
+
+        public static MigraDoc.DocumentObjectModel.Color colorDGVaTable(System.Drawing.Color color)
+        {
+            return Color.FromArgb(color.A, color.R, color.G, color.B);
+        }
+    }
+}
